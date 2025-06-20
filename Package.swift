a0920495ed665c8cf81bf63dd1ae40ca502e2fd4
dyerlab <rodney.dyer@gmail.com>
2025-06-20 09:29:13 -0400

    updating

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ import PackageDescription
 
 let package = Package(
     name: "DLabSpatial",
-    platforms: [ .macOS("13"),
+    platforms: [ .macOS(.v14),
                  .iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
